c7e093787d1368a02253d97efa0681561d47c9d0
Alexandre <alexandre.maciele@gmail.com>
2018-07-07 11:49:08 -0300

    Sum of multiples

---
+++ sum-of-multiples/SumOfMultiples.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+public static class SumOfMultiples
+{
+    public static int Sum(IEnumerable<int> multiples, int max)
+    {
+        var multipleNumbers = MultiplesBetween(0, max, multiples);
+        if(!multipleNumbers.Any())
+        {    
+            return 0;
+        }
+
+        return multipleNumbers.Sum();
+    }
+
+    public static IEnumerable<int> MultiplesBetween(int lowerLimit, int upperLimit, IEnumerable<int> multiples)
+    {
+        var numbersBetween = NumbersBetween(lowerLimit, upperLimit);
+        return (from number in numbersBetween
+                
+                let isMultiple = (from multiple in multiples
+                                  where number % multiple == 0
+                                  select multiple)
+                
+                where isMultiple.Any() == true
+                
+                select number);
+    }
+
+    public static List<int> NumbersBetween(int lowerLimit, int upperLimit)
+    {
+        List<int> list = new List<int>();
+        
+        while(lowerLimit < upperLimit)
+        {
+            list.Add(lowerLimit);
+            lowerLimit++;
+        }
+        return list;
+    }
+}
