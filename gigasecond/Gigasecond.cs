9ec092865754720481f683d7d921438a91c8420e
Alexandre <alexandre.maciele@gmail.com>
2018-07-30 18:19:51 -0300

    Modifications suggested by mentor

--- gigasecond/Gigasecond.cs
+++ gigasecond/Gigasecond.cs
@@ -2,9 +2,9 @@ using System;
 
 public static class Gigasecond
 {
+    private const double gigaSecond = 1000000000;
     public static DateTime Add(DateTime birthDate)
     {
-        double gigaSecond = 1000000000;
         return birthDate.AddSeconds(gigaSecond);
     }
 }
