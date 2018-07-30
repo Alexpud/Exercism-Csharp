f477dcbb46094fa72754cefaae27af74b747dc27
Alexandre <alexandre.maciele@gmail.com>
2018-07-30 19:00:41 -0300

    TwoFer exercise

---
+++ two-fer/TwoFer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+  public static class TwoFer
+  {
+      public static string Name(string input = null)
+      {
+          string whoIsItFor = input == null ? "you" : input;
+          return $"One for {whoIsItFor}, one for me.";
+      }
+  }
