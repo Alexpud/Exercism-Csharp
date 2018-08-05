b6deb65a66388dab882193a38546c9837eff410d
Alexandre <alexandre.maciele@gmail.com>
2018-08-05 09:59:56 -0300

    Difference of squares

---
+++ difference-of-squares/DifferenceOfSquares.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public static class DifferenceOfSquares
+{
+    public static int CalculateSquareOfSum(int max)
+    {
+        var numbers = Enumerable.Range(0, max + 1);
+        return (int)Math.Pow(numbers.Sum(), 2);
+    }
+
+    public static int CalculateSumOfSquares(int max)
+    {
+        var numbers = Enumerable.Range(0, max + 1);
+        var numbersSquares = numbers.Select(x => Math.Pow(x, 2));
+        return (int)numbersSquares.Sum();
+    }
+
+    public static int CalculateDifferenceOfSquares(int max)
+    {
+        return CalculateSquareOfSum(max) - CalculateSumOfSquares(max);
+    }
+}
