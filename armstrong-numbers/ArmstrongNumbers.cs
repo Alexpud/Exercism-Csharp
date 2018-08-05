f8e61e41dea8fb76ac71c7898c357b845578036f
Alexandre <alexandre.maciele@gmail.com>
2018-08-05 09:22:58 -0300

    ArmstrongNumbers

---
+++ armstrong-numbers/ArmstrongNumbers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public static class ArmstrongNumbers
+{
+    public static bool IsArmstrongNumber(int number)
+    {
+        string numberString = number.ToString();
+        int numberSize = numberString.Length;
+        var digitsPows = numberString.Select(x => 
+        {
+            return Math.Pow(Char.GetNumericValue(x), numberSize);
+        });
+        
+        return digitsPows.Sum() == number;
+    }
+}
