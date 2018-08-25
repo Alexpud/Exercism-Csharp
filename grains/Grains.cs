6cd0ceeca0992a33b30e2c62dc6683ef5f6cbaf2
Alexandre <alexandre.maciele@gmail.com>
2018-08-25 20:18:13 -0300

    Grains exercise

---
+++ grains/Grains.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public static class Grains
+{
+    public static ulong Square(int n)
+    {
+        if (n > 64 || n <= 0)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+        ulong square = 1;
+        for (int i = 1; i < n; i++)
+        {
+            square *= 2UL;
+        }
+
+        return square;
+    }
+
+    public static ulong Total()
+    {
+        /*
+            Since it is the sum of a Geometric Progression of order 2, and the last element 
+            value cannot be held inside a ulong variable, this is the equivalent operation
+        */
+        var lastElementSquare = Square(64);
+        return lastElementSquare + lastElementSquare - 1;
+    }
+}
