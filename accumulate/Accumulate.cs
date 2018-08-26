d404b9732792a2b902b773db5355f93735f008e8
Alexandre <alexandre.maciele@gmail.com>
2018-08-26 11:18:16 -0300

    Accumulate exercise

---
+++ accumulate/Accumulate.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+
+public static class AccumulateExtensions
+{
+    public static IEnumerable<U> Accumulate<T, U>(this IEnumerable<T> collection, Func<T, U> func)
+    {
+        return collection.Select(x => func.Invoke(x));
+    }
+}
