cfbbdede303bd199bb7b01163df9b86a6d530b65
Alexandre <alexandre.maciele@gmail.com>
2018-09-01 18:49:45 -0300

    Suggested changes

--- accumulate/Accumulate.cs
+++ accumulate/Accumulate.cs
@@ -7,10 +7,9 @@ public static class AccumulateExtensions
 {
     public static IEnumerable<U> Accumulate<T, U>(this IEnumerable<T> collection, Func<T, U> func)
     {
-        List<U> newCollection = new List<U>();
         foreach(var element in collection)
         {
-            yield return func.Invoke(element);
+            yield return func(element);
         }
     }
 }
