3dfd4c3e1384755c23d582559e818a84568ed7b7
alexpud <alexandre.maciele@gmail.com>
2018-12-25 11:24:37 -0200

    ScrabbleScore exercise

--- scrabble-score/ScrabbleScore.cs
+++ scrabble-score/ScrabbleScore.cs
@@ -3,38 +3,43 @@ using System.Collections.Generic;
 
 public static class ScrabbleScore
 {
-    private enum Alphabet
+    private static readonly Dictionary<string, int> dict = new Dictionary<string, int>()
     {
-        A = 1,
-        B = 3,
-        C = 3,
-        D = 2,
-        E = 1,
-        F = 4,
-        G = 2,
-        H = 4,
-        I = 1,
-        J = 8,
-        K = 5,
-        L = 1,
-        M = 3,
-        N = 1,
-        O = 1,
-        P = 3,
-        Q = 10,
-        R = 1,
-        S = 1,
-        T = 1,
-        U = 1,
-        V = 4,
-        W = 4,
-        Y = 4,
-        X = 8,
-        Z = 10
-    }
+        {"A",  1},
+        {"B",  3},
+        {"C",  3},
+        {"D",  2},
+        {"E",  1},
+        {"F",  4},
+        {"G",  2},
+        {"H",  4},
+        {"I",  1},
+        {"J",  8},
+        {"K",  5},
+        {"L",  1},
+        {"M",  3},
+        {"N",  1},
+        {"O",  1},
+        {"P",  3},
+        {"Q",  10},
+        {"R",  1},
+        {"S",  1},
+        {"T",  1},
+        {"U",  1},
+        {"V",  4},
+        {"W",  4},
+        {"Y",  4},
+        {"X",  8},
+        {"Z",  10}
+    };
 
     public static int Score(string input)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        int sum = 0;
+        foreach(var letter in input.ToUpper())
+        {
+            sum += dict[letter.ToString()];
+        }
+        return sum;
     }
 }
