d9478d63efad70714f6d1ae816f7f1a67ff22476
Alexandre <alexandre.maciele@gmail.com>
2019-02-23 09:47:55 -0300

    Resistor encoding exercise

---
+++ resistor-color/ResistorColor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public static class ResistorColor
+{
+    private enum ResistorEncoding
+    {
+        black = 0,
+        brown = 1,
+        red = 2,
+        orange = 3,
+        yellow = 4,
+        green = 5,
+        blue = 6,
+        violet = 7,
+        grey = 8,
+        white = 9,
+    }
+
+    public static int ColorCode(string color)
+    {
+        return (int)Enum.Parse(typeof(ResistorEncoding), color.ToLower());
+    }
+
+    public static string[] Colors()
+    {
+        return Enum.GetNames(typeof(ResistorEncoding));
+    }
+}
