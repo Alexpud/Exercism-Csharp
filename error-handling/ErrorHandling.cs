dd99d526611fa1bb2f6545ce7da44e30eb2a9600
Alexandre <alexandre.maciele@gmail.com>
2019-03-03 11:03:46 -0300

    Solution to error handling exercise

---
+++ error-handling/ErrorHandling.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public static class ErrorHandling
+{
+    public static void HandleErrorByThrowingException()
+    {
+        throw new Exception();
+    }
+
+    public static int? HandleErrorByReturningNullableType(string input)
+    {
+        return Char.IsDigit(input[0]) ? (int?)Int32.Parse(input) : null;
+    }
+
+    public static bool HandleErrorWithOutParam(string input, out int result)
+    {
+        result = HandleErrorByReturningNullableType(input) ?? 0;
+        return result == 0 ? false : true;
+    }
+
+    public static void DisposableResourcesAreDisposedWhenExceptionIsThrown(IDisposable disposableObject)
+    {
+        disposableObject.Dispose();
+        throw new Exception();
+    }
+}
