22be9fb2bfef452761dd077ce84dc0834bd552e5
Alexandre <alexandre.maciele@gmail.com>
2019-03-13 23:13:36 -0300

    QueenAttack and robotSimulator

---
+++ queen-attack/QueenAttack.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class Queen
+{
+    public Queen(int row, int column)
+    {
+        if (row < 0 || column < 0)
+        {
+            throw new ArgumentOutOfRangeException("Queen position can not be negative");
+        }
+
+        if (row >= 8 || column >= 8)
+        {
+            throw new ArgumentOutOfRangeException("Queen position must not be outside the board");
+        }
+
+        Row = row;
+        Column = column;
+    }
+
+    public int Row { get; }
+    public int Column { get; }
+}
+
+public static class QueenAttack
+{
+    public static bool CanAttack(Queen white, Queen black)
+    {
+        bool sameDiagonal = Math.Abs(white.Row - black.Row) == Math.Abs(white.Column - black.Column);
+        bool sameLine = (white.Row == black.Row);
+        bool sameColumn = (white.Column == black.Column);
+        return sameDiagonal || sameLine || sameColumn;
+    }
+
+    public static Queen Create(int row, int column)
+    {
+        return new Queen(row, column);
+    }
+}
---
+++ robot-simulator/RobotSimulator.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+public enum Direction
+{
+    North,
+    East,
+    South,
+    West
+}
+
+public class RobotSimulator
+{
+    public RobotSimulator(Direction direction, int x, int y)
+    {
+        _direction = direction;
+        _x = x;
+        _y = y;
+    }
+
+    private Direction _direction;
+
+    public Direction Direction
+    {
+        get
+        {
+            return _direction;
+        }
+    }
+
+    private int _x;
+
+    public int X
+    {
+        get
+        {
+            return _x;
+        }
+    }
+
+    private int _y;
+
+    public int Y
+    {
+        get
+        {
+            return _y;
+        }
+    }
+
+    public void Move(string instructions)
+    {
+        foreach (var instruction in instructions)
+        {
+            switch (instruction)
+            {
+                case 'R':
+                    MoveRight();
+                    break;
+
+                case 'L':
+                    MoveLeft();
+                    break;
+
+                case 'A':
+                    MoveForward();
+                    break;
+            }
+        }
+    }
+
+    private void MoveRight()
+    {
+        int value = (int)_direction + 1;
+        bool clockWiseCircle = value >= Enum.GetValues(typeof(Direction)).Length;
+        if (clockWiseCircle)
+        {
+            value = 0;
+        }
+        Enum.TryParse(value.ToString(), out _direction);
+    }
+
+    private void MoveLeft()
+    {
+        int value = (int)_direction - 1;
+        bool counterClockWiseCircle = value < 0;
+        if (counterClockWiseCircle)
+        {
+            value = Enum.GetValues(typeof(Direction)).Length - 1;
+        }
+        Enum.TryParse(value.ToString(), out _direction);
+    }
+
+    private void MoveForward()
+    {
+        if (_direction == Direction.South || _direction == Direction.North)
+        {
+            _y += _direction == Direction.South ? -1 :
+                _direction == Direction.North ? 1 : 0;
+            return;
+        }
+        
+        _x += _direction == Direction.East ? 1 :
+            _direction == Direction.West ? -1 : 0;
+    }
+
+}
