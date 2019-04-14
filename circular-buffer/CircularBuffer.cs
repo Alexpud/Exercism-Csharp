19e0118fb359918ed18fb58c0ff0d3618a357313
Alexandre <alexandre.maciele@gmail.com>
2019-04-14 20:00:33 -0300

    An approach using pointers instead of an array

--- circular-buffer/CircularBuffer.cs
+++ circular-buffer/CircularBuffer.cs
@@ -4,69 +4,86 @@ using System.Collections.Generic;
 
 public class CircularBuffer<T>
 {
+    private Node<T> head;
+    private Node<T> tail;
+    public int length;
     private readonly int capacity;
-    private int insertPosition;
-    private int readPosition;
-    private T[] buffer;
-    private int length;
-
+    
     public CircularBuffer(int capacity)
     {
         this.capacity = capacity;
-        buffer = new T[capacity];
     }
 
     public T Read()
     {
-        if (length == 0)
+        if (head == null)
         {
             throw new InvalidOperationException("Cant read from an empty buffer");
         }
 
-        readPosition %= this.capacity;
-        if (readPosition >= capacity)
-        {
-            throw new InvalidOperationException("An item is only readable once");
-        }
-
-        this.length--;
-        return buffer[readPosition++];
+        var value = head.Value;
+        head = head.Next;
+        length--;
+        return value;
     }
 
     public void Write(T value)
     {
-        bool bufferIsFull = this.length == this.capacity;
-        if (bufferIsFull)
+        if(length == capacity)
         {
-            throw new InvalidOperationException("Cant write to a full buffer");
+            throw new InvalidOperationException("Buffer is at maximum capacity");
         }
 
-        insertPosition %= this.capacity;
-        buffer[insertPosition++] = value;
-        this.length++;
+        if (head == null)
+        {
+            head = new Node<T>(value);
+            tail = head;
+            length++;
+            return;
+        }
+        
+        FindPositionAndInsertElement(value);
+    }
+
+    private void FindPositionAndInsertElement(T value)
+    {
+        Node<T> currentElement = tail;
+        var newNode = new Node<T>(value);
+        length++;
+        if (length == capacity)
+        {
+            newNode.Next = head;
+        }
+        currentElement.Next = newNode;
+        tail = currentElement.Next;
     }
 
     public void Overwrite(T value)
     {
-        bool bufferIsFull = this.length == this.capacity;
-        if (bufferIsFull)
+        if (length != capacity)
         {
-            insertPosition %= this.capacity;
-            buffer[insertPosition] = value;
-            readPosition++;
+            Write(value);
+            return;
         }
-        else
-        {
-            buffer[insertPosition++] = value;
-            this.length++;
-        }
+        head.Value = value;
+        head = head.Next;
     }
 
     public void Clear()
     {
-        buffer = new T[capacity];
-        this.length = 0;
-        insertPosition = 0;
-        readPosition = 0;
+        head = null;
+        length = 0;
+    }
+
+    private class Node<T> 
+    {
+        public T Value { get; set; }
+        public Node<T> Next { get; set; }
+
+        public Node(T value)
+        {
+            this.Value = value;
+            this.Next = null;
+        }
     }
 }
