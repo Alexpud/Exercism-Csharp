e8087728e0fd11adbfd114d882270afeb2039376
Alexandre <alexandre.maciele@gmail.com>
2019-04-19 18:39:39 -0300

    Refactoring of Acronym

--- acronym/Acronym.cs
+++ acronym/Acronym.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Text;
 
 public static class Acronym
@@ -6,15 +7,11 @@ public static class Acronym
   	private static readonly char [] wordDelimiters = { ' ', '-', '_'};
     public static string Abbreviate(string phrase)
     {
-    	var words = phrase.Split(wordDelimiters);
-    	StringBuilder acronym = new StringBuilder();
-    	foreach(var word in words)
-    	{
-    		if (!string.IsNullOrEmpty(word))
-    		{
-    			acronym.Append(Char.ToUpper(word[0]));
-    		}
-    	}
-    	return acronym.ToString();
+		var initials = phrase.Split(wordDelimiters)
+				   	   		.Where(word => !string.IsNullOrEmpty(word))
+				   			.Select(word => Char.ToUpper(word[0]))
+				   			.ToArray();
+
+		return new String(initials);
     }
 }
