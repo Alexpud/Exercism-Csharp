b9f610ba324fcf90bb24ef8911f7b3ff384921f2
Alexandre <alexandre.maciele@gmail.com>
2019-04-19 18:34:15 -0300

    Acronym exercise

---
+++ acronym/Acronym.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+public static class Acronym
+{
+  	private static readonly char [] wordDelimiters = { ' ', '-', '_'};
+    public static string Abbreviate(string phrase)
+    {
+    	var words = phrase.Split(wordDelimiters);
+    	StringBuilder acronym = new StringBuilder();
+    	foreach(var word in words)
+    	{
+    		if (!string.IsNullOrEmpty(word))
+    		{
+    			acronym.Append(Char.ToUpper(word[0]));
+    		}
+    	}
+    	return acronym.ToString();
+    }
+}
