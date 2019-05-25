ac4dd5f4f91e1c9f053a9b83968082fe68988ea9
Alexandre <alexandre.maciele@gmail.com>
2019-05-25 10:48:53 -0300

    Meetup exercise

--- meetup/Meetup.cs
+++ meetup/Meetup.cs
@@ -12,12 +12,66 @@ public enum Schedule
 
 public class Meetup
 {
+    private readonly DateTime previousMeetupDate;
     public Meetup(int month, int year)
     {
+        previousMeetupDate = new DateTime(year, month, 1);
     }
 
     public DateTime Day(DayOfWeek dayOfWeek, Schedule schedule)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        DateTime baseDateForNextMeetup = CalculateBaseDateForNextMeetup(previousMeetupDate, schedule);
+        int requiredOcurrences = RequiredOcurrences(schedule);
+        return CalculateNextMeetupDate(baseDateForNextMeetup, requiredOcurrences, dayOfWeek);
+    }
+
+    private DateTime CalculateBaseDateForNextMeetup(DateTime previousMeetupDate, Schedule schedule)
+    {
+        if (schedule == Schedule.Teenth)
+        {
+            return previousMeetupDate.AddDays(12);
+        }
+
+        if (schedule == Schedule.Last)
+        {
+            var daysInMonth = DateTime.DaysInMonth(previousMeetupDate.Year, previousMeetupDate.Month);
+            return previousMeetupDate.AddDays(daysInMonth - 7);
+        }
+
+        return previousMeetupDate; 
+    }
+
+    private int RequiredOcurrences(Schedule schedule)
+    {
+        switch (schedule)
+        {
+            case Schedule.Second:
+                return 2;
+            case Schedule.Third:
+                return 3;
+            case Schedule.Fourth:
+                return 4;
+            default:
+                return 1;
+        }
+    }
+
+    private DateTime CalculateNextMeetupDate(DateTime baseDateForNextMeetup, int requiredOcurrences,
+        DayOfWeek dayOfWeek)
+    {
+        DateTime nextMeetupDate = baseDateForNextMeetup;
+        while (true)
+        {
+            if (nextMeetupDate.DayOfWeek == dayOfWeek)
+            {
+                requiredOcurrences--;
+            }
+            if (requiredOcurrences == 0)
+            {
+                break;
+            }
+            nextMeetupDate = nextMeetupDate.AddDays(1);
+        }
+        return nextMeetupDate;
     }
 }
