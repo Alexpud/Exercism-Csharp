02df16506ffee7fce6e0e42b1fe759dd362a2274
Alexandre <alexandre.maciele@gmail.com>
2019-09-10 21:13:53 -0300

    Matrix (#1)
    * FIrst commit of the branch
    
    * firs solution
    
    * refactoring
    
    * matrix exercise

---
+++ matrix/Matrix.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+static class StringExtensions
+{
+    public static string[] SplitByLineBreak(this string input)
+    {
+        return input.Split("\n");
+    }
+
+    public static string[] SplitByEmptySpace(this string input)
+    {
+        return input.Split(" ");
+    }
+}
+
+public class Matrix
+{
+    private int[][] _matrix;
+    public Matrix(string input)
+    {
+        var crudeMatrixLines = input.SplitByLineBreak();
+        var matrixSize = crudeMatrixLines.Count();
+        _matrix = new int[matrixSize][];
+        for(int i = 0; i < matrixSize; i++)
+        {
+            _matrix[i] = ConvtertToMatrixRow(crudeMatrixLines[i]);
+        }
+    }
+
+    private int[] ConvtertToMatrixRow(string line)
+    {
+        return line.SplitByEmptySpace()
+            .Select(number => Convert.ToInt32(number))
+            .ToArray();
+    }
+
+    public int Rows
+    {
+        get
+        {
+            throw new NotImplementedException("You need to implement this function.");
+        }
+    }
+
+    public int Cols
+    {
+        get
+        {
+            throw new NotImplementedException("You need to implement this function.");
+        }
+    }
+
+    public int[] Row(int row)
+    {
+        return _matrix[row - 1];
+    }
+
+    public int[] Column(int col)
+    {
+        var column = new int[_matrix.Length];
+        for(int i = 0; i < _matrix.Length; i++)
+        {
+            column[i] = _matrix[i][col - 1];
+        }
+        return column;
+    }
+}
---
+++ simple-linked-list/SimpleLinkedList.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+namespace SimpleLinkedList
+{
+    public class SimpleLinkedList<T> : IEnumerable<T>
+    {
+        private Node<T> _head;
+        private IEnumerator<T> _enumerator;
+        public SimpleLinkedList(T value)
+        {
+            _head = new Node<T>(value);
+            _enumerator = new SimpleLinkedListEnumerator<T>(_head);
+        }
+
+        public SimpleLinkedList(IEnumerable<T> values)
+        {
+            throw new NotImplementedException("You need to implement this function.");
+        }
+
+        public T Value
+        {
+            get
+            {
+                return _enumerator.Current;
+            }
+        }
+
+        public SimpleLinkedList<T> Next
+        {
+            get
+            {
+                if (_enumerator.MoveNext())
+                    return this;
+                return null;
+            }
+        }
+
+        public SimpleLinkedList<T> Add(T value)
+        {
+            _head.Next = new Node<T>(value);
+            return this;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new SimpleLinkedListEnumerator<T>(_head);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+
+    public class Node<T>
+    {
+        public T Value;
+        public Node<T> Next;
+
+        public Node(T value)
+        {
+            Value = value;
+        }
+    }
+
+    public class SimpleLinkedListEnumerator<T> : IEnumerator<T>
+    {
+        private Node<T> _head;
+        private Node<T> _current;
+        object IEnumerator.Current => _current.Value;
+        T IEnumerator<T>.Current => _current.Value;
+
+        public SimpleLinkedListEnumerator(Node<T> node)
+        {
+            _head = node;
+            _current = node;
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public bool MoveNext()
+        {
+            if (_current.Next != null)
+            {
+                _current = _current.Next;
+                return true;
+            }
+            return false;
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
