64f73a687d619d3c59aebf2cc8f57d705e353147
Alexandre <alexandre.maciele@gmail.com>
2019-10-02 20:37:08 -0300

    Largest series product (#2)
    * First unit test
    
    * Refactoring and some unit tests
    
    * few implementations
    
    * Minor refactoring

---
+++ largest-series-product/LargestSeriesProduct.cs
@@ -0,0 +1,55 @@
+using System;
+
+public static class LargestSeriesProduct
+{
+    public static long GetLargestProduct(string digits, int span)
+    {
+        if (span == 0)
+        {
+            return 1;
+        }
+
+        ThrowIfInvalidInputs(digits, span);
+
+        long largestProduct = 0;
+        long currentSpanProduct = 1;
+        for (int i = 0; i <= digits.Length - span; i++)
+        {
+            currentSpanProduct = 1;
+            for (int j = i; j <= i + (span - 1); j++)
+            {
+                var digit = (long)char.GetNumericValue(digits[j]);
+                ThrowIfInvalidDigit(digit);
+                currentSpanProduct *= digit;
+            }
+            largestProduct = currentSpanProduct > largestProduct ? currentSpanProduct : largestProduct;
+        }
+        return largestProduct;
+    }
+
+    private static void ThrowIfInvalidDigit(long digit)
+    {
+        if (digit < 0)
+        {
+            throw new ArgumentException("Can't have non digit characters");
+        }
+    }
+
+    private static void ThrowIfInvalidInputs(string digits, int span)
+    {
+        if (string.IsNullOrWhiteSpace(digits))
+        {
+            throw new ArgumentException("Digits can't be null or empty");
+        }
+
+        if (span < 0)
+        {
+            throw new ArgumentException("Span can't be negative");
+        }
+
+        if (digits.Length < span)
+        {
+            throw new ArgumentException("Span can't be larger than the digits");
+        }
+    }
+}
