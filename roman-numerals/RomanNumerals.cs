413fb1ca4ed2adc18cd95a2b26d0fde586da47cf
Alexandre <alexandre.maciele@gmail.com>
2020-04-15 19:57:26 -0300

    Roman numerals (#6)
    * Roman Numeral exercise
    
    * Minor refactoring
    
    * jusst do it

---
+++ roman-numerals/RomanNumerals.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+
+public static class RomanNumeralExtension
+{
+    private static Dictionary<int, string> arabicByRomanAlgarisms = new Dictionary<int, string>()
+    {
+        { 1000, "M" },
+        { 900, "CM" },
+        { 500, "D" },
+        { 400, "CD" },
+        { 100, "C" },
+        { 90, "XC" },
+        { 50, "L" },
+        { 40, "XL" },
+        { 20, "XX"},
+        { 10, "X"},
+        { 9, "IX" },
+        { 5, "V" },
+        { 4, "IV" },
+        { 1, "I" }
+    };
+
+    public static string ToRoman(this int value)
+    {
+        var sb = new StringBuilder();
+        foreach(var arabicAlgarism in arabicByRomanAlgarisms.Keys)
+        {
+            if (arabicAlgarism <= value)
+            {
+                var romanAlgarismCount = GetRomanAlgarismCount(value, arabicAlgarism);
+                value -= romanAlgarismCount * arabicAlgarism;
+                sb.Append(string.Concat(romanAlgarismCount));
+            }
+        }
+        return sb.ToString();
+    }
+
+    private static int GetRomanAlgarismCount(int value, int arabicAlgarism)
+    {
+        return Enumerable.Repeat(arabicByRomanAlgarisms[arabicAlgarism], value / arabicAlgarism).Count();
+    }
+}
