37295a7b5987b03c9d30a3a6033645bc369e830e
Alexpud <alexandre.maciele@gmail.com>
2020-08-24 19:56:30 -0300

    Flatten array

---
+++ flatten-array/FlattenArray.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+public static class FlattenArray
+{
+    public static IEnumerable Flatten(IEnumerable input)
+    { 
+        var inputList = input.Cast<object>().ToList();
+        var flattenedList = new List<object>();
+        if (!inputList.Any()) return flattenedList;
+        var firstElement = inputList.First();
+        if (firstElement is Array)
+        {
+            var flattennedSubList = Flatten((IEnumerable)inputList.First());
+            flattenedList = flattenedList.Concat(Flatten(flattennedSubList).Cast<object>()).ToList();
+        }     
+        else if (firstElement != null)
+        {
+            flattenedList.Add(inputList.First());
+        }
+        
+        var inputListTail = inputList.Skip(1);
+        flattenedList = flattenedList.Concat((IEnumerable<object>)Flatten(inputListTail)).ToList();
+
+        return flattenedList;
+    }
+}
