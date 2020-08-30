2e3ba1d94a50b20a68e7dc5b705749b16d508056
Alexpud <alexandre.maciele@gmail.com>
2020-08-29 23:07:03 -0300

    Improved version of frogley

--- flatten-array/FlattenArray.cs
+++ flatten-array/FlattenArray.cs
@@ -1,38 +1,23 @@
-﻿using System;
-using System.Linq;
+﻿
 using System.Collections;
-using System.Collections.Generic;
 
 public static class FlattenArray
 {
     public static IEnumerable Flatten(IEnumerable input)
     {
-        var inputList = input.Cast<object>();
-        var flattenedInput = Enumerable.Empty<object>();
-        if (!inputList.Any()) return flattenedInput;
-        var firstElement = inputList.First();
-        if (IsCollection(firstElement))
+        foreach(var element in input)
         {
-            var firstElementAsList = (IEnumerable<object>)firstElement;
-            flattenedInput = flattenedInput.Concat(FlattenList(firstElementAsList));
+            if (element is IEnumerable enumerable)
+            {
+                foreach(var el in Flatten(enumerable))
+                {
+                    yield return el;
+                }
+            }
+            else if (!(element is null))
+            {
+                yield return element;
+            }
         }
-        else if (firstElement != null)
-        {
-            flattenedInput = flattenedInput.Append(firstElement);
-        }
-
-        flattenedInput = flattenedInput.Concat(FlattenList(inputList.Skip(1)));
-        return flattenedInput;
-    }
-
-    private static bool IsCollection(object firstElement)
-    {
-        return firstElement is Array;
-    }
-
-    private static IEnumerable<object> FlattenList(IEnumerable inputListTail)
-    {
-        var flattenedInputListTail = Flatten(inputListTail);
-        return flattenedInputListTail.Cast<object>();
     }
 }
