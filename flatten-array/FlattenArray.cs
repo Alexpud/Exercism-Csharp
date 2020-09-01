61aabbefe5c885fdaad56464323d56290a59e288
Alexandre <alexandre.maciele@gmail.com>
2020-08-31 21:41:31 -0300

    Flatten array (#8)
    * Flatten array
    
    * Refactorings
    
    * Refactoring
    
    * Refactoring
    
    * Improved version of frogley
    
    * Added to the solution

---
+++ flatten-array/FlattenArray.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Collections;
+
+public static class FlattenArray
+{
+    public static IEnumerable Flatten(IEnumerable input)
+    {
+        foreach(var element in input)
+        {
+            if (element is IEnumerable enumerable)
+            {
+                foreach(var el in Flatten(enumerable))
+                {
+                    yield return el;
+                }
+            }
+            else if (!(element is null))
+            {
+                yield return element;
+            }
+        }
+    }
+}
